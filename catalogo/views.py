52ec0b95387bec6d050c88e7504c98e656c1f623
Karen Cordoba <karen_cochi_02@hotmail.com>
2023-10-19 12:17:28 -0300

    creación de templates

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import CustomUserCreationForm
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>¡Hola mundo!</h1>')
 
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'registrarion/register.html', {'form': CustomUserCreationForm})
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password']
+            )
+            login(request, user)
+
+            return redirect('home')
+        else:
+            return render(request, 'registration/register.html', {"form":form})
