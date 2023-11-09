b612d32dbf762623706190e5119965006f67fe43
Karen Cordoba <karen_cochi_02@hotmail.com>
2023-11-08 21:09:19 -0300

    personalizacion del admin

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# from catalogo.models import Genero, Autor, Idioma, Libro, Ejemplar
+from catalogo.models import Genero, Autor, Idioma, Libro, Ejemplar
 from catalogo.models import User
 from django.contrib.auth.admin import UserAdmin
 
@@ -7,9 +7,15 @@ class UserAdmin(UserAdmin):
     pass
     list_display = ('username', 'first_name', 'last_name', 'image')
 
+class ModelNameAdmin(admin.ModelAdmin):
+    list_display = ('field1', 'field2', 'field3')
+    list_filter = ('field1', 'field2', 'field3')
+
 admin.site.register(User, UserAdmin)
-# admin.site.register(Genero)
-# admin.site.register(Autor)
-# admin.site.register(Idioma)
-# admin.site.register(Libro)
-# admin.site.register(Ejemplar)
+admin.site.register(Genero)
+admin.site.register(Autor)
+admin.site.register(Idioma)
+admin.site.register(Libro)
+admin.site.register(Ejemplar)
+# admin.site.register(ModelNameAdmin)
+# MyClass,
